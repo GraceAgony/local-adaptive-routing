05986274614b34694c157c15207b529755d831c2
GraceAgony <theogonia8@gmail.com>
2018-11-22 09:45:41 +0300

    add text

--- local-adaptive-routing/src/components/Graph.jsx
+++ local-adaptive-routing/src/components/Graph.jsx
@@ -7,37 +7,55 @@ class MyGraph extends Component {
         super(props);
 
         this.state = {
-            currentNodeId : 0,
+            currentNodeId : 1,
+            currentTextId : 0,
             data : {
-                nodes: [{id: 'Harry'}, {id: 'Sally'}, {id: 'Alice'}],
-                links: [{source: 'Harry', target: 'Sally'}, {source: 'Harry', target: 'Alice'}]
+                nodes: [{id: '0'}],
+                links: []
             },
             nodesDeleting: false,
             linksDeleting: false,
             linksAdding: false,
             nodesChosen: [],
+            nodesPlaces : {
+
+            }
         };
 
         this.myConfig = {
                 nodeHighlightBehavior: true,
+                linkHighlightBehavior: true,
+                maxZoom:0,
+                minZoom: 0,
                 node: {
                     color: 'purple',
-                    size: 120,
-                    highlightStrokeColor: 'blue'
+                    size: 300,
+                    highlightStrokeColor: 'blue',
+                    fontSize:15,
+                    highlightFontSize: 15
                 },
                 link: {
-                    highlightColor: 'lightblue'
+                    highlightColor: 'lightblue',
+                    strokeWidth: 4,
+                    color: 'green'
                 }
             };
     }
 
     onClickGraph =  () => {
        // window.alert(`Clicked the graph background`);
+        this.someChanges();
     };
 
     onClickNode =(nodeId) => {
         if(this.state.data.nodes.length === 1){return}
        if(this.state.nodesDeleting){
+           let links = this.state.data.links.filter((link)=>
+               String(link.source)===nodeId || String(link.target)===nodeId);
+           links.map((link)=>{
+               document.getElementById('text'+link.text.id).remove();
+               return 0;
+           });
            this.setState({data :
                    Object.assign(this.state.data,
                        {nodes: this.state.data.nodes.filter((value)=>String(value.id)!==nodeId),
@@ -48,10 +66,9 @@ class MyGraph extends Component {
            let newNode = {id: nodeId};
 
            if (this.state.nodesChosen.length > 0) {
-               for (let i = 0; i < this.state.nodesChosen; i++) {
+               for (let i = 0; i < this.state.nodesChosen.length; i++) {
                    console.log(this.state.nodesChosen[i].id);
-                   if (this.state.nodesChosen[i].id == nodeId) {
-                       console.log('same');
+                   if (this.state.nodesChosen[i].id === nodeId) {
                        return
                    }
                }
@@ -61,6 +78,7 @@ class MyGraph extends Component {
                this.addLink();
            }
        }
+        this.someChanges();
     };
 
     onRightClickNode =(event, nodeId)=> {
@@ -68,26 +86,93 @@ class MyGraph extends Component {
     };
 
     onMouseOverNode = (nodeId) => {
-        //window.alert(`Mouse over node ${nodeId}`);
+       this.someChanges();
     };
 
+
     onMouseOutNode =  (nodeId) => {
-       // window.alert(`Mouse out node ${nodeId}`);
+        this.someChanges();
     };
+
+    someChanges = () => {
+        let nodesChanged = {};
+        for(let i =0; i<this.state.data.nodes.length; i++) {
+            let id = this.state.data.nodes[i].id;
+            let elem = document.getElementById(id);
+            let cx = elem.getAttribute('cx');
+            let cy = elem.getAttribute('cy');
+            if (id in this.state.nodesPlaces) {
+                if ((this.state.nodesPlaces[id].cx === cx) &&
+                    (this.state.nodesPlaces[id].cy === cy)) {
+                    continue;
+                }
+            }
+            nodesChanged[id]=id;
+            this.setState({
+                nodesPlaces: Object.assign(this.state.nodesPlaces, {[id]:
+                        {cx: cx, cy: cy}})
+            })
+        }
+                for(let i=0; i< this.state.data.links.length; i++){
+                    let link = this.state.data.links[i];
+                    if(link.source in nodesChanged || link.target in nodesChanged){
+                        let text = document.getElementById('text'+link.text.id);
+                        let targetX = parseFloat(this.state.nodesPlaces[link.target].cx);
+                        let targetY = parseFloat(this.state.nodesPlaces[link.target].cy);
+                        let sourceX = parseFloat(this.state.nodesPlaces[link.source].cx);
+                        let sourceY = parseFloat(this.state.nodesPlaces[link.source].cy);
+                        let Xmax;
+                        let Xmin;
+                        let Ymax;
+                        let Ymin;
+                        if(targetX>sourceX){
+                            Xmax=targetX;
+                            Xmin=sourceX;
+                        }else {
+                            Xmax=sourceX;
+                            Xmin=targetX;
+                        }
+                        if(targetY>sourceY){
+                            Ymax=targetY;
+                            Ymin=sourceY;
+                        }else {
+                            Ymax=sourceY;
+                            Ymin=targetY;
+                        };
+                        let x = Xmin+(Xmax-Xmin)/2;
+                        let y= Ymin+(Ymax-Ymin)/2;
+                        if(text){
+                            text.style.left = x+'px';
+                            text.style.top = y+'px';
+                        }else {
+                            let newElem=document.createElement('div');
+                            newElem.setAttribute('id', 'text'+link.text.id);
+                            newElem.style.left = x+'px';
+                            newElem.style.top = y+'px';
+                            newElem.style.position = "absolute";
+                            newElem.innerText = link.text.value;
+                            document.getElementById("graph-id-graph-wrapper").appendChild(newElem);
+                        }
+                    }}
+        };
 
     onClickLink =  (source, target) => {
         console.log(`linkClicked source: ${source} target: ${target}`);
             if(this.state.linksDeleting){
-             let newLinks =  this.state.data.links.filter((link)=>{
-                 console.log(link);
-                 return (link.source!==source || link.target!==target)&&
-                 (link.source!==target || link.target!==source)}).map(
-                     (value => new Object({target: value.target, source: value.source})));
+                let links = this.state.data.links.filter(
+                    (link)=>(link.source === source && link.target === target)||
+                        (link.source === target && link.target === source));
+                links.map((link)=>{
+                    document.getElementById('text'+link.text.id).remove();
+                    return 0;
+                });
             this.setState({data :
                     Object.assign(this.state.data,
-                        {links: newLinks})});
-            console.log(this.state.data.links);
+                        {links: this.state.data.links.filter(
+                            (link)=>(link.source!==source || link.target!==target)&&
+                                (link.source!==target || link.target!==source))})})
         }
+        this.someChanges();
     };
 
     onRightClickLink =  (event, source, target) => {
@@ -96,10 +181,12 @@ class MyGraph extends Component {
 
     onMouseOverLink =  (source, target) => {
        // window.alert(`Mouse over in link between ${source} and ${target}`);
+        this.someChanges();
     };
 
     onMouseOutLink =  (source, target) => {
        // window.alert(`Mouse out link between ${source} and ${target}`);
+        this.someChanges();
     };
 
     handlerAddNode= () =>{
@@ -112,6 +199,7 @@ class MyGraph extends Component {
 
     handlerDeleteNode =()=>{
         this.setState({nodesDeleting: true});
+        this.someChanges();
     };
 
     handlerStopDeletingNode=()=>{
@@ -129,7 +217,17 @@ class MyGraph extends Component {
 
 
     addLink = () =>{
-        let newLink =  {source: this.state.nodesChosen[0].id, target: this.state.nodesChosen[1].id};
+        let id=this.state.currentTextId;
+        if(this.state.nodesChosen[0].id === this.state.nodesChosen[1].id){
+            let arr = [this.state.nodesChosen[0].id];
+            this.setState({
+                nodesChosen: arr,
+            });
+            return;
+        }
+        let newLink =  {source: this.state.nodesChosen[0].id, target: this.state.nodesChosen[1].id,
+            text:{value: (Math.random()*100).toFixed(0), id: "text"+id}
+        };
         for(let i=0; i<this.state.data.links.length; i++){
             if((this.state.data.links[i].source===newLink.source)&&
                 (this.state.data.links[i].target===newLink.target)){
@@ -138,9 +236,10 @@ class MyGraph extends Component {
         }
         this.setState({
             data : Object.assign(this.state.data, this.state.data.links.push(newLink)),
-            nodesChosen: []
+            nodesChosen: [],
+            currentTextId: id+1
         });
-        this.forceUpdate();
+        this.someChanges();
     }
 
     handlerDeleteLink=()=>{
@@ -150,8 +249,11 @@ class MyGraph extends Component {
     }
 
     handlerStopDeletingLink=() =>{
-
+        this.setState({
+            linksDeleting: false
+        })
     }
+
 
     render() {
         return (
